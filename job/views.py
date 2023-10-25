129f2788c061811591f1aa4cdc62059e739392c5
Mohamed <m7mdellham77@gmail.com>
2023-10-26 00:33:35 +0300

    add jobs list

--- job/views.py
+++ job/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import job
+
 
 # Create your views here.
+
+
+def job_list(request):
+    all_jobs=job.objects.all()
+    return render(request,'job/job_list.html',{'jobs':all_jobs})
